bca86a551b1e5ad0659ee88df7152cb45d569d70
Felix Perktold <felix@perktold.xyz>
2021-06-19 20:19:18 +0200

    shit hacked together shittily works well (hopefully)

--- start.lisp
+++ start.lisp
@@ -2,46 +2,73 @@
 (ql:quickload :aserve)
 (ql:quickload :cl-mysql)
 (ql:quickload :cl-ppcre)
+(ql:quickload :cl-json)
 
 (net.aserve:start)
 (cl-mysql:connect)
 (cl-mysql:query "USE chan")
 
+(net.aserve:publish-file :path "/favicon.ico" :file "/root/favicon.ico")
+(net.aserve:publish-directory :prefix "/css" :destination "/root/42chan/css")
+
 (net.aserve:publish :path "/"
 		    :content-type "text/html"
-		    :function 'gen-helloworld)
+		    :function 'gen-index)
+
+(net.aserve:publish :path "/api"
+		    :content-type "text/json"
+		    :function 'gen-json)
 
 (net.aserve:publish :path "/post"
 		    :content-type "text/html"
-		    :function 'gen-helloworld)
+		    :function 'gen-board)
 
-(net.aserve:publish :path "/boards/g"
+(net.aserve:publish :path "/view"
 		    :content-type "text/html"
-		    :function 'gen-gview)
+		    :function 'gen-board)
 
+(defun gen-board (req ent)
+  (let ((post (net.aserve:request-query-value "post" req)) 
+        (board (net.aserve:request-query-value "board" req))
+	(thread (net.aserve:request-query-value "thread" req)))
+
+  (net.aserve:with-http-response (req ent)
+    (net.aserve:with-http-body (req ent)
+      (gen-boardlist)
+      (display-post-form board thread)
+      (if (and post (not (equal post "")))
+	(new-post board thread post))
+
+      (if (or (not thread) (equal thread ""))
+	(display-posts (caar (cl-mysql:query (format nil "SELECT id,board,content FROM Posts WHERE board = \"~A\" AND OPid IS NULL" board))))
+	(display-posts (caar (cl-mysql:query (format nil "SELECT id,board,content FROM Posts WHERE board = \"~A\" AND OPid = ~A" board thread)))))
+) ) ) )
+
+(defun display-posts (posts)
+  (loop for p in posts do
+    (net.html.generator:html
+      ((:div class "post")
+        (:hr)
+	((:a id (car p) href (format nil "view?board=~A&thread=~A" (cadr p) (car p))) (:princ-safe (car p)))
+	(:p (:princ-safe (caddr p)))
+) ) ) )
+	      
 (defun gen-boardlist ()
   (net.html.generator:html
+  ((:link :rel "stylesheet" :href "css/style.css"))
   ((:div class "boards")
     (loop for b in (caar (cl-mysql:query "SELECT * FROM Boards")) do
       (net.html.generator:html
-        ((:a href (car b)) (:princ-safe (car b)))
+        ((:a href (format nil "view?board=~A" (car b))) (:princ-safe (car b)))
 ) ) ) ) )
-	      
-(defun gen-gview (req ent)
-  (net.aserve:with-http-response (req ent)
-    (net.aserve:with-http-body (req ent)
-      (loop for p in (caar (cl-mysql:query "SELECT id,content FROM Posts WHERE board = \"g\" AND OPid IS NULL")) do
-        (net.html.generator:html
-	  ((:div class "post")
-	   (:hr)
-	   ((:a id (car p)) (:princ-safe (car p)))
-	   (:br)
-	   (:princ-safe (format nil "~{~A~}" (cdr p)))
-) ) ) ) ) )
 
-(defun display-post-form()
+(defun display-post-form (board thread)
   (net.html.generator:html
-    ((:form :action "post" :method "post")
+    ((:form :action 
+	    (if (and thread (not (equal thread "")))
+	      (format nil "view?board=~A&thread=~A" board thread)
+	      (format nil "view?board=~A" board))
+	    :method "post")
      ((:textarea
               :name "post"
               :placeholder "shitpost (2048 characters max)"
@@ -49,30 +76,30 @@
               :cols 45
               :maxlength 2048))
      (:br)
-     ((:input :type "text"
-              :name "pswd"
-              :placeholder "password for deleting this post later"))
      ((:input :type "submit"
               :value "post")))
 ) )
 
-(defun new-post (post pswd)
+(defun new-post (board thread post)
   (cl-mysql:query "ALTER TABLE Posts AUTO_INCREMENT = 1;")
   (handler-case
-    (if (equal pswd "")
-      (cl-mysql:query (format nil "INSERT INTO Posts VALUES(\"g\", NULL, \"~A\", NULL, NULL, NULL, NULL);" (cl-ppcre:regex-replace-all "\"" post "\\\"")))
-      (cl-mysql:query (format nil "INSERT INTO Posts VALUES(null, \"~A\", \"~A\");" (cl-ppcre:regex-replace-all "\"" post "\\\"")
-                                                                                        (cl-ppcre:regex-replace-all "\"" pswd "\\\""))))
+    (cl-mysql:query 
+      (if (and thread (not (equal thread "")))
+	(format nil "INSERT INTO Posts VALUES(\"~A\", \"~A\", \"~A\", NULL, NULL, NULL, NULL);"
+			      (cl-ppcre:regex-replace-all "\"" board "\\\"")
+			      (cl-ppcre:regex-replace-all "\"" thread "\\\"")
+			      (cl-ppcre:regex-replace-all "\"" post "\\\""))
+	(format nil "INSERT INTO Posts VALUES(\"~A\", NULL, \"~A\", NULL, NULL, NULL, NULL);"
+			      (cl-ppcre:regex-replace-all "\"" board "\\\"")
+			      (cl-ppcre:regex-replace-all "\"" post "\\\""))))
     (condition (c)))
   (net.html.generator:html (:html
-    (:head (:title "posted: " (:princ-safe pswd)))
+    (:head (:title "posted: " (:princ-safe post)))
       (:body
         (:p "posted: " (:b (:princ-safe post))
 ) ) ) ) )
 
-(defun gen-helloworld (req ent)
-  (let ((post (net.aserve:request-query-value "post" req))
-	(pswd (net.aserve:request-query-value "pswd" req)))
+(defun gen-index (req ent)
   (net.aserve:with-http-response (req ent)
     (net.aserve:with-http-body (req ent)
       (net.html.generator:html
@@ -80,13 +107,11 @@
 	  (:head
 	    (:title
 	      "test")
+	    ((:link :rel "stylesheet" :href "css/style.css"))
 	    (:body
 	      (gen-boardlist)
 	      (:p (:small "the time is "
 			  (:princ-safe (get-universal-time))))
-	      (:h1 "hello world")
-	      (display-post-form)
-	      (if (and post (not (equal post "")))
-		(new-post post pswd))
-) ) ) ) ) ) ) )
-
+	      (:h1 "42chan")
+	      (display-posts (caar (cl-mysql:query "SELECT id,board,content FROM Posts" )))
+) ) ) ) ) ) )
